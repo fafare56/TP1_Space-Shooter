fced37270f66c9440ca2ea9d11b127ba223272b2
fafare56 <fancheventravail@gmail.com>
2025-09-21 20:03:13 -0400

    changer vies pour confilt

--- Source/TP1FanchEven/Private/Vaisseau.cpp
+++ Source/TP1FanchEven/Private/Vaisseau.cpp
@@ -29,7 +29,7 @@ AVaisseau::AVaisseau()
 	MovementComponent->UpdatedComponent = RootComponent;
 
 	// Vies
-	Vies = 3;
+	Vies = 5;
 	CurrentMoveX = 0;
 	CurrentMoveY = 0;
 	CurrentShootDirection = FVector(1,0,0); // par défaut droite
